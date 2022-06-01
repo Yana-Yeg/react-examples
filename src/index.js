227b82c0da8a2a8a44dccb478aa2edd4df31a4d5
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-06-01 22:10:30 +0200

    store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
