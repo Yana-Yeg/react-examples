1ef0b1a8190ca272ac2e0ec1e9a7fa1f4c674328
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-06-07 23:10:48 +0200

    style

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Icons from "../images/sprite.svg";
 
 const PostsPage = () => {
   const [posts, setPosts] = useState([]);
@@ -12,11 +13,20 @@ const PostsPage = () => {
 
   return (
     <div>
-      <h1>Our news</h1>
+      <h1 style={{ position: "relative" }}>
+        Our news
+        <svg className="postsPage_svg_news">
+          <use xlinkHref={`${Icons}#icon-news`} />
+        </svg>
+      </h1>
       <ul className="postsPage_ul">
         {posts &&
           posts.map((post) => (
             <li key={post.id} className="postsPage_li">
+              <svg className="postsPage_svg_post">
+                <use xlinkHref={`${Icons}#icon-doc`} />
+              </svg>
+
               <Link to={`/posts/${post.id}`}>{post.title} </Link>
             </li>
           ))}
