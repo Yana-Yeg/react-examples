68caf1cdc92878101a4905ce54b12f3a83d99348
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-06-08 09:17:15 +0200

    add filter

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+import PostFilter from "../components/PostFilter/PostFilter";
 import Icons from "../images/sprite.svg";
 
 const PostsPage = () => {
   const [posts, setPosts] = useState([]);
+  const [serchParams, setSerchParams] = useSearchParams();
+
+  const postQuery = serchParams.get("post") || "";
+  const latest = serchParams.has("latest");
+  const startsFrom = latest ? 80 : 1;
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -19,17 +25,26 @@ const PostsPage = () => {
           <use xlinkHref={`${Icons}#icon-news`} />
         </svg>
       </h1>
+      <PostFilter
+        postQuery={postQuery}
+        latest={latest}
+        setSerchParams={setSerchParams}
+      />
       <ul className="postsPage_ul">
         {posts &&
-          posts.map((post) => (
-            <li key={post.id} className="postsPage_li">
-              <svg className="postsPage_svg_post">
-                <use xlinkHref={`${Icons}#icon-doc`} />
-              </svg>
+          posts
+            .filter(
+              (post) => post.title.includes(postQuery) && post.id >= startsFrom
+            )
+            .map((post) => (
+              <li key={post.id} className="postsPage_li">
+                <svg className="postsPage_svg_post">
+                  <use xlinkHref={`${Icons}#icon-doc`} />
+                </svg>
 
-              <Link to={`/posts/${post.id}`}>{post.title} </Link>
-            </li>
-          ))}
+                <Link to={`/posts/${post.id}`}>{post.title} </Link>
+              </li>
+            ))}
       </ul>
     </div>
   );
